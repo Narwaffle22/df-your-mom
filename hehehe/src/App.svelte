d8da3fbeff025fefafd88ac1f1865cd3b5b30d4e
Dlol <dravennadas@gmail.com>
2022-10-03 11:55:37 -0500

    github pages #15

---
+++ hehehe/src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+	import Section from "./Section.svelte";
+	import Footer from "./Footer.svelte"
+</script>
+
+<main>
+	<h1>df your mom</h1>
+	<Section title="What is df your mom">
+		<p>It is a project hoping to improve the developer experience of clients worldwide</p>
+	</Section>
+	<Section title="What does it do?">
+		<p>What doesnt it do?</p>
+		<h3>well..</h3>
+		<ul>
+			<li>everything</li>
+		</ul>
+	</Section>
+	<Footer></Footer>
+</main>
+
+<style>
+	/* main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	} */
+</style>
---
+++ hehehe/src/Footer.svelte
@@ -0,0 +1,16 @@
+<footer>
+    <hr>
+    contact us!
+    <br>
+    dont.
+</footer>
+
+<style>
+    footer {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        text-align: center;
+    }
+</style>
